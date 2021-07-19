0c1d90c8d05fe7a88a0f2bbfbc18d2513eb0c1b7
Joe Robinson <joe.robinson@krminc.com>
2021-07-19 15:47:36 -0400

    Mocha 9 and Yarn Berry Bug * Adding basic yarn, mocha, and chai to show bug with Mocha 9 and Yarn 2

---
+++ test-mocha-and-chai.spec.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('Testing Mocha 9.0.2 with Yarn 2.4.2', () => {
+  it('Should have 1 test that passes', () => {
+    expect(true).to.be.true;
+  });
+});
